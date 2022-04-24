06905f0a6a82b6898d4f9adfa27901a159e7c938
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2022-04-24 16:20:58 -0400

    Add breakfast sandwich

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -103,6 +103,18 @@ function Menu() {
           </ul>
           
         </fieldset>
+
+        <fieldset>
+
+          <h1>{data.menu[3].title}</h1>
+          <p>{data.menu[3].description}</p>
+          <ul>
+            {data.menu[3].breakfastSandwiches.map(breakfastSandwich => (
+              <li key={breakfastSandwich.id}>{breakfastSandwich[0]} - {breakfastSandwich[1]}</li>
+            ))}
+          </ul>
+          
+        </fieldset>
       </main>
     );
   }
