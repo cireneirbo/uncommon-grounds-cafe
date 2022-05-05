6d9ff3801a6bbef9dd3cef9f601a31180f17ca1a
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2022-05-05 17:42:47 -0400

    More attempts at making chaning background

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,29 @@ import Footer from "./components/Footer";
 import './App.css';
 
 
-const images = ['../../public/images/fancoffeeshop.jpg', "../../public/images/chattycoffeshop.jpg", "../public/images/npccoffeeshop.jpg"];
-let index = 0;
 
-function buildImage() {
-    document.getElementById('body').style.backgroundImage = 'url('+images[index]+')';
-}
-
-function changeImage() {
-    index++;
-    if (index >= images.length) index = 0;
-    document.getElementById('body').style.backgroundImage = 'url(' + images[index] + ')';
-}
 function App() {
+
+
+  const images = ['../public/images/fancycoffeeshop.jpg', "../public/images/chattycoffeshop.jpg", "../public/images/npccoffeeshop.jpg"];
+  let index = 0;
+
+  // function buildImage() {
+  //     document.getElementById('body').style.backgroundImage = 'url('+images[index]+')';
+  // }
+
+  function changeImage() {
+      index++;
+      if (index >= images.length) index = 0;
+      document.getElementsByClassName('body').style.backgroundImage = 'url(' + images[index] + ')';
+  }
+
   return (
     <div class="body">
       <Nav />
       <Outlet />
       <Footer />
-      <button onclick="changeImage()">NextImage</button>
+      <button onclick={changeImage}>NextImage</button>
     </div>
   );
 }
