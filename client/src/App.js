5897841d2152a41098082ee56f40f5e49f24a19e
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2022-05-08 12:45:35 -0400

    COmment out the backgroundUpdate code

--- client/src/App.js
+++ client/src/App.js
@@ -3,32 +3,16 @@ import Nav from './components/Nav';
 import Footer from "./components/Footer";
 import './App.css';
 
-
-
 function App() {
-
-
-  const images = ['../public/images/fancycoffeeshop.jpg', "../public/images/chattycoffeshop.jpg", "../public/images/npccoffeeshop.jpg"];
-  let index = 0;
-
-  // function buildImage() {
-  //     document.getElementById('body').style.backgroundImage = 'url('+images[index]+')';
-  // }
-
-  function changeImage() {
-      index++;
-      if (index >= images.length) index = 0;
-      document.getElementsByClassName('body').style.backgroundImage = 'url(' + images[index] + ')';
-  }
 
   return (
     <div class="body">
       <Nav />
       <Outlet />
       <Footer />
-      <button onclick={changeImage}>NextImage</button>
     </div>
   );
+
 }
 
 export default App;
--- client/src/utilities/backgroundUpdate.js
+++ client/src/utilities/backgroundUpdate.js
@@ -1,15 +1,16 @@
-const images = ['../../public/images/fancoffeeshop.jpg', "../public/images/chattycoffeshop.jpg", "../public/images/npccoffeeshop.jpg"];
-let index = 0;
+// const images = ['../../public/images/fancoffeeshop.jpg', "../public/images/chattycoffeshop.jpg", "../public/images/npccoffeeshop.jpg"];
+// let index = 0;
 
-function buildImage() {
-    document.getElementById('body').style.backgroundImage = 'url('+images[index]+')';
-}
+// function buildImage() {
+//     document.getElementById('body').style.backgroundImage = 'url('+images[index]+')';
+// }
 
-function changeImage() {
-    index++;
-    if (index >= images.length) index = 0;
-    document.getElementById('body').style.backgroundImage = 'url(' + images[index] + ')';
-}/*
+// function changeImage() {
+//     index++;
+//     if (index >= images.length) index = 0;
+//     document.getElementById('body').style.backgroundImage = 'url(' + images[index] + ')';
+// }
+/*
 .contents {
     width: 200px;
     height: 200px;
