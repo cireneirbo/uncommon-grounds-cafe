fa2f165876ae892653568fd887f3476c3fc6e1a8
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2022-05-12 19:04:29 -0400

    Contact page

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -7,10 +7,19 @@ function Contact() {
 
         <h1>Contact Us</h1>
 
+        <h3>Phone</h3>
+          <p>
+            (941) 123-4567
+          </p>
+        <h3>Address</h3>
+          <p>
+            501 S Beneva Rd Sarasota, FL 34232
+          </p>
+
         <iframe 
           width="100%" 
           height="600" 
-          src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;coord=52.70967533219885, -8.020019531250002&amp;q=1%20Grafton%20Street%2C%20Dublin%2C%20Ireland&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" 
+          src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;coord=52.70967533219885, -8.020019531250002&amp;q=501%20Beneva%20Road%2C%20Sarasota%2C%20USA&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" 
           frameBorder="0" 
           scrolling="no" 
           marginHeight="0" 
