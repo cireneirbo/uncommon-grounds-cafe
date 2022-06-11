e210f7872ad5cec2da16f7d8320cf06a7d0a6bc7
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2022-06-11 19:58:17 -0400

    Update footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,23 +8,23 @@ function Footer() {
 
         <ul className="footer"> 
           <li>
-            <Link to="/" className="footer-link">Home</Link>
+            <Link to="" className="footer-link">Home</Link>
           </li>
             <li> | </li>
           <li>
-            <Link to="/menu" className="footer-link">Menu</Link>
+            <Link to="menu" className="footer-link">Menu</Link>
           </li>
             <li> | </li>
           <li>
-            <Link to="/about" className="footer-link">About</Link>
+            <Link to="about" className="footer-link">About</Link>
           </li>
             <li> | </li>
           <li>
-            <Link to="/hiring" className="footer-link">Hiring</Link>
+            <Link to="hiring" className="footer-link">Hiring</Link>
           </li>
             <li> | </li>
           <li>
-            <Link to="/contact" className="footer-link">Contact</Link>
+            <Link to="contact" className="footer-link">Contact</Link>
           </li>
         </ul>
       </div>
